255be02628ab50cc382d5b4fb31f1e7c437573d4
celsoaramos <celsoaramos@gmail.com>
2020-06-08 08:58:20 -0300

    inicio-componente-desafio

---
+++ projetos/componente-intro/src/Contadores.vue
@@ -0,0 +1,30 @@
+<template>
+
+  <div class="contadores">
+    <h1>Contadores</h1>
+    <app-contador v-for="c in 5" :key="c"></app-contador>
+    <button>?</button>
+  </div>
+
+
+</template>
+
+<script>
+// registro de forma local o componente Contador
+import Contador from './Contador.vue'
+
+export default {
+    
+    components: {
+      'app-contador': Contador
+    },
+
+    
+
+}
+</script>
+
+<style scoped>
+
+
+</style>
--- projetos/componente-intro/src/main.js
+++ projetos/componente-intro/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Contadores from './Contadores.vue'
 
 Vue.config.productionTip = false
+
+// registro de forma global o componente Contadores
+Vue.component('app-contadores', Contadores)
 
 new Vue({
   render: h => h(App),
