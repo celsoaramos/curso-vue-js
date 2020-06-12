7e0dd303ebfc9a53a9f70dad364d039ad56ed6ea
Celso Ramos <celsoaramos@gmail.com>
2020-06-12 14:34:07 -0300

    desafio slot

---
+++ projetos/componente-avancado-citacoes/src/components/Citacao.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="citacao">
+        
+        <slot></slot><!-- autor -->
+        <slot name="texto"></slot>
+        
+        <div class="fonte">
+            <slot name="fonte"></slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+   
+    created() {
+        console.log('created')
+    },
+    destroyed() {
+        console.log('destroyed')
+    },
+    activated() {
+        console.log('activated')
+    },
+    destroyed() {
+        console.log('destroyed')
+    }
+
+
+}
+</script>
+
+<style scoped>
+    .citacao {
+        border: 1px solid #DDD;
+        background-color: rgba(0, 0, 0, .1);
+        padding: 25px;
+        max-width: 450px;
+    }
+
+    .fonte {
+        float: right;
+    }
+</style>
